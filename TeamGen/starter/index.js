776ed48e705c8ed1cc8a24919425b27dcc4555ea
Sean Stevenson <sean.flynn.stevenson@gmail.com>
2023-03-05 18:24:43 +0000

    All tests working

--- TeamGen/starter/index.js
+++ TeamGen/starter/index.js
@@ -11,37 +11,137 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
+if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+}
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer
-    .prompt([
+const team = [];
+
+promptManager();
+
+function promptManager() {
+    inquirer.prompt([
         {
-            type: 'input',
-            name: 'name',
+            type: "input",
+            name: "name",
             message: "What is the team manager's name?",
         },
         {
-            type: 'input',
-            name: 'id',
+            type: "input",
+            name: "id",
             message: "What is the team manager's employee ID?",
         },
         {
-            type: 'input',
-            name: 'email',
+            type: "input",
+            name: "email",
             message: "What is the team manager's email address?",
         },
         {
-            type: 'input',
-            name: 'officeNumber',
+            type: "input",
+            name: "officeNumber",
             message: "What is the team manager's office number?",
         },
     ])
-    .then((answers) => {
-        const manager = new Manager(
-            answers.name,
-            answers.id,
-            answers.email,
-            answers.officeNumber
-        );
-        console.log(manager);
-    });
+        .then((answers) => {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            team.push(manager);
+            promptTeamMembers();
+        });
+}
+
+// Function to prompt user for team member information
+function promptTeamMembers() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "New Team Member?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finish building my team",
+            ],
+        },
+    ])
+        .then((answer) => {
+            switch (answer.role) {
+                case "Engineer":
+                    promptEngineer();
+                    break;
+                case "Intern":
+                    promptIntern();
+                    break;
+                case "Finish building my team":
+                    const html = render(team);
+                    fs.writeFileSync(outputPath, html);
+                    open(outputPath);
+                    break;
+            }
+        });
+}
+
+// Function to prompt user for engineer information
+function promptEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's employee ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's GitHub username?",
+        },
+    ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            team.push(engineer);
+            promptTeamMembers();
+        });
+}
+
+// Function to prompt user for intern information
+function promptIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's employee ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the intern's school?",
+        },
+    ])
+        .then((answers) => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            team.push(intern);
+            promptTeamMembers();
+        })
+};
+
+fs.writeFileSync(outputPath, render(team));
--- TeamGen/starter/src/page-template.js
+++ TeamGen/starter/src/page-template.js
@@ -13,7 +13,7 @@ const generateTeam = team => {
             <ul class="list-group">
                 <li class="list-group-item">ID: ${manager.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                <li class="list-group-item">Office number: ${manager.getOffice()}</li>
             </ul>
         </div>
     </div>
@@ -58,6 +58,7 @@ const generateTeam = team => {
         `;
     };
 
+    function render(team)
     const html = [];
 
     html.push(team
